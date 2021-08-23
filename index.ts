2a0ef33162abbbaef4388546de35b01a46b51bb9
CanRau <cansrau@gmail.com>
2021-08-22 19:52:41 -0500

    cleanup for issue

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
-// todo: testing https://github.com/pulumi/pulumi/issues/2260
-// https://aws.amazon.com/about-aws/whats-new/2018/04/aws-sam-cli-launches-new-commands/
-
-// import { readdirSync } from "fs";
-// import { join } from "path";
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-// import * as awsx from "@pulumi/awsx";
-// import * as mime from "mime";
 import { handler } from "./www/server/lambda";
 
 const config = new pulumi.Config("aws");
@@ -31,79 +24,15 @@ new aws.iam.RolePolicyAttachment("lambda-basic-execution", {
   role: role.name,
 });
 
-// const lambda = new aws.lambda.Function("handler", {
-//   code: new pulumi.asset.FileArchive("./dist/main.zip"),
-//   role: role.arn,
-//   handler: "main.handler",
-//   runtime: "nodejs14.x",
-// });
-
-// const lambdaLayer = new aws.lambda.L
 const lambda = new aws.lambda.CallbackFunction(
   "handler",
   {
     callback: handler,
-    // callbackFactory: () => handler,
     role,
     runtime: "nodejs14.x",
-    // layers: []
   },
   providerOpts,
 );
 
-// const bucket = new aws.s3.Bucket("bucket", {}, providerOpts);
-// const handlerObject = new aws.s3.BucketObject(
-//   "handler.zip",
-//   {
-//     bucket,
-//     source: new pulumi.asset.FileArchive("./www"),
-//     contentType: "application/zip",
-//   },
-//   { replaceOnChanges: ["source"], ...providerOpts },
-// );
-
-// const lambda = new aws.lambda.Function(
-//   "handler",
-//   {
-//     // code: new pulumi.asset.FileArchive("./www"),
-//     role: role.arn,
-//     handler: "server/lambda.handler",
-//     runtime: "nodejs14.x",
-//     s3Bucket: bucket.arn,
-//     s3Key: handlerObject.key,
-//   },
-//   providerOpts,
-// );
-
 export const lambdaInvokeArn = lambda.invokeArn;
 export const lambdaArn = lambda.arn;
-
-// // Create an AWS resource (S3 Bucket)
-// const bucket = new aws.s3.Bucket("canrau.com-prod", {
-//   website: {
-//     indexDocument: "index.html",
-//   },
-// });
-
-// // const bucketObject = new aws.s3.BucketObject("index.html", {
-// //   acl: "public-read",
-// //   contentType: "text/html",
-// //   bucket: bucket,
-// //   source: new pulumi.asset.FileAsset("index.html")
-// // });
-
-// let siteDir = "www/dist/client"; // directory for content files
-
-// // For each file in the directory, create an S3 object stored in `siteBucket`
-// for (let item of readdirSync(siteDir)) {
-//   let filePath = join(siteDir, item);
-//   let object = new aws.s3.BucketObject(item, {
-//     bucket: bucket,
-//     source: new pulumi.asset.FileAsset(filePath), // use FileAsset to point to a file
-//     contentType: mime.getType(filePath) || undefined, // set the MIME type of the file
-//   });
-// }
-
-// // Export the name of the bucket
-// export const bucketName = bucket.id;
-// export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
--- www/server/lambda.ts
+++ www/server/lambda.ts
@@ -1,66 +1,19 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { createPageRender } from "vite-plugin-ssr";
-// import * as vite from "vite";
-// import * as fs from "fs";
-// import { join } from "path";
-// import helmet from "helmet";
-// import { cspHashes } from "@vitejs/plugin-legacy";
+import * as SSR from "vite-plugin-ssr";
 
-// require("vite-plugin-ssr"); // just for testing purposes
-
-import "../dist/server/importBuild"; // extension necessary?
-
-// const root = `${__dirname}/..`;
-
-// type StaticFileHandlerProps = {
-//   url: string;
-//   isBase64Encoded: boolean;
-// };
-// const staticFileHandler = async ({
-//   url,
-//   isBase64Encoded,
-// }: StaticFileHandlerProps): Promise<string | Error> =>
-//   new Promise((resolve, reject) => {
-//     const buf: any = [];
-//     const stream = fs.createReadStream(join(root, "dist", "client", url));
-//     stream.on("error", (err) => {
-//       reject(err);
-//     });
-//     stream.on("open", () => {});
-//     stream.on("data", (chunk) => buf.push(chunk));
-//     stream.on("end", () => {
-//       const bodyBuffer = Buffer.concat(buf);
-//       resolve(bodyBuffer.toString(isBase64Encoded ? "base64" : "utf8"));
-//     });
-//   });
+import "../dist/server/importBuild";
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult | undefined> => {
   const url = event.path;
   if (event.httpMethod.toLowerCase() === "get") {
-    // if (/\.{css|js(on)?}$/.test(url)) {}
-    // const staticFile = await staticFileHandler({ url, isBase64Encoded: event.isBase64Encoded });
+    const renderPage = SSR.createPageRender({ isProduction: true });
 
-    // if (typeof staticFile === "string") {
-    //   return {
-    //     statusCode: 200,
-    //     body: staticFile,
-    //     isBase64Encoded: event.isBase64Encoded,
-    //   };
-    // }
-
-    const renderPage = createPageRender({ isProduction: true });
-
-    console.log("SERVER req.originalUrl", { url });
     const rawSlug = url.replace(/^\/+|\/+$/g, "");
     const [locale, ...slugParts] = rawSlug.split("/");
     const slug = slugParts.join("/");
-    console.log({ url, slug, slugParts, locale });
-    // todo: how to ensure slug part is a locale & supported?
-    // todo: MAYBE move all DB logic in here?
-    // https://en.wikipedia.org/wiki/IETF_language_tag
-    // https://www.ietf.org/rfc/bcp/bcp47.txt "en"/"de" = simple language subtag
+
     const contextProps: {
       errors: string[];
       url: string;
@@ -69,7 +22,6 @@ export const handler = async (
       slugParts: string[];
     } = { url, locale, slug, slugParts, errors: [] };
     if (slugParts.length < 1) {
-      // todo: redirect to browser language or sthg
       contextProps.errors = [...(contextProps.errors ?? []), "ERROR_LANGUAGE_MISSING"];
     }
     const result = await renderPage({ url, contextProps });
@@ -80,17 +32,12 @@ export const handler = async (
         body: "<h1>404</h1>",
         isBase64Encoded: false,
       };
-      // if redirectTo
     }
 
-    // if (result?.renderResult?.redirectTo) {
-    //   res.redirect(307, "/movie/add");
-    // } else {
     return {
       statusCode: result.statusCode,
       body: result.renderResult as string,
     };
-    // }
   }
 
   return {
@@ -98,19 +45,4 @@ export const handler = async (
     body: "<h1>404</h1>",
     isBase64Encoded: false,
   };
-  // more at https://content-security-policy.com/
-  // app.use(
-  //   helmet({
-  //     contentSecurityPolicy: {
-  //       directives: {
-  //         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-  //         "script-src": [
-  //           "self",
-  //           // !isProduction && "unsafe-inline",
-  //           ...cspHashes.map((s) => `sha256-${s}`),
-  //         ],
-  //       },
-  //     },
-  //   })
-  // );
 };
